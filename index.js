bf5ccc95ccdebca2aa861f80003c9ada2bd840d0
artur-kompala <artur.kompala@gmail.com>
2023-12-06 00:03:05 +0100

    api

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express()
 const {port} = require('./config')
 const apiRouter = require('./routes/api')
-
+const bodyParser = require('body-parser')
+//db
 require('./db/mongoose')
+//parser
+app.use(bodyParser.json())
+//routes
+app.use('/api/',apiRouter)
 
-app.use('/',apiRouter)
-
+//server
 app.listen(port,function(){
     console.log("Serwer słucha http://localhost:" + port);
 });
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const movieActions = require('../actions/api/movie')
+const movieActions = require('../actions/api/movieActions.js')
 
 
-router.get('/', movieActions.newMovie) 
-
+router.get('/movies', movieActions.getAllMovies) 
+router.get('/movies/:id', movieActions.getMovie) 
+router.post('/movies', movieActions.newMovie) 
+router.put('/movies:id', movieActions.updateMovie) 
+router.delete('/movies:id', movieActions.deleteMovie) 
 
 
 module.exports = router;
